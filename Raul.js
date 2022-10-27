b0259ab80dbb1926a5d270b71fb870310cb2bf81
Raulzip <51215786+Raulzip@users.noreply.github.com>
2022-10-27 11:52:01 -0300

    Create Raul.js

---
+++ Raul.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Quero uma camiseta</h1>
+    <h3>Quando chega?</h3>
+    <alert>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad est, dignissimos ipsum cum beatae molestias dolor
+        reiciendis aut cupiditate, consectetur quibusdam! Est fugit numquam, quam doloremque eaque nisi reprehenderit
+        sequi!</alert>
+
+</body>
+
+</html>
