afa77d56fa02c011e9d8dc3ad07692a14d839d89
Renanpjl <116854152+Renanpjl@users.noreply.github.com>
2022-10-27 22:48:59 -0300

    Create renan.js

---
+++ renan.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h1>Meu site pra hack</h1>
+    <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore animi distinctio culpa, quas omnis
+        deleniti ad amet commodi dolore minima quisquam dicta excepturi eos voluptatibus obcaecati et, earum nobis
+        perspiciatis.</span>
+    <h2>Obrigado por tudo!</h2>
+        
+</body>
+
+</html>
